7d271cff9e466287cd90fdbaeb0dc4f9722461f3
Albertiy <2409205116@qq.com>
2021-09-02 13:16:27 +0800

    [2021-09-02 13:16:14] 完成录音棚页面效果

--- front_server/pages/recordaudio.js
+++ front_server/pages/recordaudio.js
@@ -1,8 +1,16 @@
 import Head from "next/head";
+import { useState } from "react";
 import TitleBar from "../src/component/title_bar";
 import styles from '../styles/recordaudio.module.scss'
+import Icon from "@mdi/react";
+import { mdiPlay, mdiCheckBold, mdiMicrophone } from '@mdi/js';
+
+const defaultTimeInfo = '00:00';
 
 function RecordAudio() {
+
+    const [timeInfo, setTimeInfo] = useState(defaultTimeInfo);
+
     return (
         <div className={styles.container}>
             <Head>
@@ -15,13 +23,27 @@ function RecordAudio() {
             </header>
             <main className={styles.main}>
                 <div className={styles.cover}>
-                    <img className={styles.cover_image}></img>
+                    <img className={styles.cover_image} src="img/turntable.png" alt='封面图' title='封面'></img>
                 </div>
                 <div className={styles.btn_panel}>
-
+                    <div className={styles.btn}>
+                        <div className={styles.icon}>
+                            <Icon path={mdiPlay} size={1} /></div>
+                        <div>预览</div>
+                    </div>
+                    <div className={styles.btn}>
+                        <div className={styles.icon}>
+                            <Icon path={mdiMicrophone} size={1} /></div>
+                        <div>录音</div>
+                    </div>
+                    <div className={styles.btn}>
+                        <div className={styles.icon}>
+                            <Icon path={mdiCheckBold} size={1} /></div>
+                        <div>完成</div>
+                    </div>
                 </div>
                 <div className={styles.info}>
-
+                    <div>时长：{timeInfo}</div>
                 </div>
             </main>
             <footer>
