d9a00debd2a33a6a27afd5376c6fe64eae535c3f
Albertiy <2409205116@qq.com>
2021-09-03 09:16:08 +0800

    [2021-09-03 09:15:50] H5Example添加web拍照

--- H5example/server.js
+++ H5example/server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     // response.end('Hello World\n'); // 返回 response
     getIndex(res);  // 返回 index.html
-}).listen(8521); // 指定监听端口
+}).listen(8523); // 指定监听端口
 
 function getIndex(res) {
     fs.readFile('index.html', (err, data) => {
@@ -15,4 +15,4 @@ function getIndex(res) {
     })
 }
 
-console.log('Server running at http://127.0.0.1:8521');
+console.log('Server running at http://127.0.0.1:8523');
