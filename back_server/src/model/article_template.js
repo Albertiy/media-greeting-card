8c619afdf4f4b91372d74044f5f8555fb3915901
Albertiy <2409205116@qq.com>
2021-10-11 15:33:19 +0800

    [2021-10-11 15:33:08] 完善 article_template 模型

--- back_server/src/model/article_template.js
+++ back_server/src/model/article_template.js
@@ -1,14 +1,20 @@
 class ArticleTemplate {
     id;
+    name;
     skeleton;
+    selectable;
+    order;
 
     /**
      * 
      * @param {number} id 
      * @param {object} skeleton 
      */
-    constructor(id, skeleton) {
+    constructor(id, name, skeleton, selectable, order) {
         this.id = id;
+        this.name = name;
         this.skeleton = skeleton;
+        this.selectable = selectable;
+        this.order = order;
     }
 }
