66b3e3cab7dc64c157df3754c42fcd9e6f4b5b6c
Albertiy <2409205116@qq.com>
2021-10-11 15:21:46 +0800

    add uploadfiles.access_pwd

--- database/test.sql
+++ database/test.sql
@@ -49,4 +49,8 @@ set @c = convert('[{"type":"image", "id":1}]',json);
 select @c;
 select JSON_ARRAY(@c);
 
+select * from music where product_id = ? order by `order` is null, `order`asc;
+select * from bgimage;
+select * from product;
+
 
