cb388fac5c32c60b340dbd9aad09941a26ca8127
Albertiy <2409205116@qq.com>
2021-10-15 16:02:42 +0800

    [2021-10-15 16:02:33] 音乐播放组件完成

--- front_server/src/component/background_music/background_music.js
+++ front_server/src/component/background_music/background_music.js
@@ -24,13 +24,33 @@ function BackgroundMusic(props) {
     }, [musicOn])
 
     return (
-        <div className={[styles.music_btn, musicOn ? styles.music_on_btn : undefined].join(" ")} onClick={onClick}>
-            <Icon path={musicOn ? mdiMusic : mdiMusicOff} size={0.85}></Icon>
-            {/* autoPlay={true} */}
-            <audio controls={false} loop ref={audioEle} src={source}>
-                {/* <source src={source}></source> */}
-            </audio>
-        </div>
+        <div className={styles.container}>
+            <div className={[styles.music_btn, musicOn ? styles.music_on_btn : undefined].join(" ")} onClick={onClick}>
+                <Icon path={musicOn ? mdiMusic : mdiMusicOff} size={0.85}></Icon>
+                {/* autoPlay={true} */}
+                <audio controls={false} loop ref={audioEle} src={source}>
+                    {/* <source src={source}></source> */}
+                </audio>
+            </div>
+            <div className={styles.effect} style={{ filter: "url(#gooey)" }}>
+                <div className={styles.moon}></div>
+                <span className={[styles.satellite_ball, styles.ball_1, musicOn ? styles.satellite_ball_on : undefined].join(" ")} style={{ zIndex: 1, animationDuration: "2s", }}></span>
+                <span className={[styles.satellite_ball, styles.ball_2, musicOn ? styles.satellite_ball_on : undefined].join(" ")} style={{ zIndex: 1, animationDuration: "30s", }}></span>
+                <svg className={styles.effect_svg}>
+                    <filter id="gooey">
+                        {/* <!-- 高斯模糊，由整个图像创建效果，模糊程度为10 --> */}
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
+                        {/* <!-- 色彩矩阵，R|G|B|A|常亮偏移 * R|G|B|A --> */}
+                        <feColorMatrix values="
+                            1 0 0 0 0
+                            0 1 0 0 0
+                            0 0 1 0 0
+                            0 0 0 20 -10
+                            " />
+                    </filter>
+                </svg>
+            </div>
+        </div >
     );
 }
 
