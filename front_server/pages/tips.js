f724fec29c4f1860d751e484926d9268d3cd490b
Albertiy <2409205116@qq.com>
2021-10-21 16:55:33 +0800

    [2021-10-21 16:55:22] 温馨提示页面添加侧边栏

--- front_server/pages/tips.js
+++ front_server/pages/tips.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import Head from 'next/head';
+import router from 'next/router';
+import { useSnackbar } from 'notistack';
+import FloatSidebar from '../src/component/float_sidebar/FloatSidebar';
+import useCode from '../src/hook/useCode';
+import GlobalSettings from '../src/setting/global';
+import styles from '../styles/at_1_manage.module.scss';
 
 export default function tips() {
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const { code, routerRefreshCount, routerLoaded } = useCode()
+
     return (
-        <div>
-            <p>温馨提示</p>
+        <div className={styles.container}>
+            <Head>
+                <title>{GlobalSettings.siteTitle('温馨提示')}</title>
+                <meta charSet='utf-8' />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <header>
+            </header>
+            <main className={styles.main}>
+                <div className={styles.contentLayer}>
+                    <section className={styles.tipsContainer}>
+                        <p>温馨提示</p>
+                    </section>
+                </div>
+                <div className={styles.upperLayer}>
+                    <section className={styles.menuBtnContainer}>
+                        <FloatSidebar onItemClicks={[function () { router.push({ pathname: '/at_1_manage', query: { code } }) },
+                        function () { router.push({ pathname: '/login_pwd', query: { code } }) },
+                        function () { router.push({ pathname: '/tips', query: { code } }) }]}></FloatSidebar>
+                    </section>
+                </div>
+            </main>
+            <footer>
+            </footer>
         </div>
     )
 }
