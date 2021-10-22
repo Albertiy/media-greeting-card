b7d8bfba312af5f07a685a64039584d67b6bfd1d
Albertiy <2409205116@qq.com>
2021-10-22 14:34:28 +0800

    [2021-10-22 14:32:57] 登录页添加回到预览页按钮

--- front_server/pages/login/index.js
+++ front_server/pages/login/index.js
@@ -1,13 +1,15 @@
+import { mdiMenu, mdiArrowLeft } from '@mdi/js';
+import { Icon } from '@mdi/react';
 import { Button, Input, Layout, Space } from 'antd'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useSnackbar } from 'notistack'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
+import ModelLoading from '../../src/component/model_loading'
 import useCode from '../../src/hook/useCode'
 import * as fileService from '../../src/service/file_service'
 import GlobalSettings from '../../src/setting/global'
 import styles from './login.module.scss'
-import ModelLoading from '../../src/component/model_loading'
 
 export default function Login() {
     const router = useRouter();
@@ -53,17 +55,29 @@ export default function Login() {
     }
 
     return (
-        <Layout className={styles.container}>
+        <div className={styles.container}>
             <Head>
                 <title>{GlobalSettings.siteTitle('登录')}</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Space direction="vertical" size="large" className={styles.form}>
-                <label className={styles.label}>请输入管理密码：</label>
-                <Input.Password className={styles.input} placeholder="密码" maxLength={20} size="large" onChange={(e) => { setPwd(e.target.value) }} />
-                <Button type="primary" block={true} className={styles.btn} size="large" onClick={loginBtnClicked}>登录</Button>
-            </Space>
-            {isLoading && <ModelLoading />}
-        </Layout>
+            <main className={styles.main}>
+                <Layout className={styles.pwdContainer}>
+                    <Space direction="vertical" size="large" className={styles.form}>
+                        <label className={styles.label}>请输入管理密码：</label>
+                        <Input.Password className={styles.input} placeholder="密码" maxLength={20} size="large" onChange={(e) => { setPwd(e.target.value) }} />
+                        <Button type="primary" block={true} className={styles.btn} size="large" onClick={loginBtnClicked}>登录</Button>
+                    </Space>
+                    {isLoading && <ModelLoading />}
+                </Layout>
+                <div className={styles.upperLayer}>
+                    <section className={styles.menuBtnContainer}>
+                        <Button className={styles.manageBtn} title="返回"
+                            onClick={() => { console.log('返回'); router.push({ pathname: '/at_1', query: { code } }) }} >
+                            <Icon path={mdiArrowLeft} size={1.0} />
+                        </Button>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
