093f1c47dbcadc5ebeec00f49f0f13734f5d6fca
Albertiy <2409205116@qq.com>
2021-10-28 09:41:55 +0800

    [2021-10-28 09:41:43] 完善tips页

--- front_server/pages/tips.js
+++ front_server/pages/tips.js
@@ -4,7 +4,7 @@ import { useCookies } from 'react-cookie';
 import FloatSidebar from '../src/component/float_sidebar/FloatSidebar';
 import useCode from '../src/hook/useCode';
 import GlobalSettings from '../src/setting/global';
-import styles from '../styles/at_1_manage.module.scss';
+import styles from '../styles/tips.module.scss';
 
 export default function Tips() {
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
@@ -23,7 +23,24 @@ export default function Tips() {
             <main className={styles.main}>
                 <div className={styles.contentLayer}>
                     <section className={styles.tipsContainer}>
-                        <p>温馨提示</p>
+                        <label className={styles.label}>温馨提示</label>
+                        <div>
+                            <p className={styles.details}>
+                                初始管理密码 123456；
+                            </p>
+                            <p className={styles.details}>
+                                加密：设置进入预览效果页面的访问密码；
+                            </p>
+                            <p className={styles.details}>
+                                默认预览：设置后，扫码将直接看到预览的效果；
+                            </p>
+                            <p className={styles.details}>
+                                左上角按钮：进入管理页，或打开侧边栏；
+                            </p>
+                            <p className={styles.details}>
+                                右上角按钮：当设置了背景音乐时，点击按钮播放或暂停BGM。
+                            </p>
+                        </div>
                     </section>
                 </div>
                 <div className={styles.upperLayer}>
